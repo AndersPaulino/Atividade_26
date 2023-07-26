ce7cb2c17a16d108ebe6a36080e194ddc0b4e2d1
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-07-26 20:56:48 -0300

    Getter/Setter e Constructor

--- Atividade/src/main/java/org/example/entity/Conta.java
+++ Atividade/src/main/java/org/example/entity/Conta.java
@@ -4,4 +4,24 @@ public class Conta {
     private String nome;
     private Integer saldo;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Integer saldo) {
+        this.saldo = saldo;
+    }
+
+    public Conta(String nome, Integer saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
 }
