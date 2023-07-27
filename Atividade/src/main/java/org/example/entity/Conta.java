5a5137f4f7fd3926a1e0832cb6db8058e6be200e
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-07-26 21:03:48 -0300

    Criação de metodos da classe Conta.

--- Atividade/src/main/java/org/example/entity/Conta.java
+++ Atividade/src/main/java/org/example/entity/Conta.java
@@ -2,7 +2,7 @@ package org.example.entity;
 
 public class Conta {
     private String nome;
-    private Integer saldo;
+    private double saldo;
 
     public String getNome() {
         return nome;
@@ -12,16 +12,36 @@ public class Conta {
         this.nome = nome;
     }
 
-    public Integer getSaldo() {
+    public double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(Integer saldo) {
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    public Conta(String nome, Integer saldo) {
+    public Conta(String nome, double saldo) {
         this.nome = nome;
         this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de " + valor + " unidades realizado com sucesso.");
+    }
+
+    public void resgatar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Resgate de " + valor + " unidades realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para realizar o resgate.");
+        }
+    }
+
+    public double getSaldoDisponivel() {
+        // Simulando investimentos com um rendimento de 5% ao mês
+        double rendimentoMensal = saldo * 0.05;
+        return saldo + rendimentoMensal;
     }
 }
