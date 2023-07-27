24274607b1684a473bffae4030a137ffbe6eadd3
Anderson_Paulino <xandeerson@gmail.com>
2023-07-26 22:48:53 -0300

    Alteração dos metodos, e criação de uma variavel cliente que recebe nova conta, e atualização das cases do menu.

--- Atividade/src/main/java/org/example/Main.java
+++ Atividade/src/main/java/org/example/Main.java
@@ -2,31 +2,30 @@ package org.example;
 
 import org.example.entity.Conta;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
+
+    static Conta cliente = new Conta("Anonimo", 100);
+
     public static void main(String[] args) {
-
-        Conta cliente = new Conta("Anonimo", 100);
-
         Scanner scanner = new Scanner(System.in);
         int opcao = 1;
 
-        while (opcao > 0){
+        while (opcao != 0) {
             menu();
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     resgate();
-                    continue;
+                    break;
                 case 2:
                     deposito();
-                    continue;
+                    break;
                 case 3:
                     visualizar();
-                    continue;
+                    break;
                 case 4:
                     exibirsaldo();
                     break;
@@ -46,36 +45,54 @@ public class Main {
 
         scanner.close();
     }
+
     public static void menu() {
-        System.out.println("Menu" + "\n");
+        System.out.println("Menu");
+        System.out.println("1 - Resgate");
+        System.out.println("2 - Depósito");
+        System.out.println("3 - Visualizar Dados da Conta");
+        System.out.println("4 - Exibir Saldo");
+        System.out.println("5 - Investimento");
+        System.out.println("6 - Bônus");
+        System.out.println("0 - Sair");
+        System.out.println("Digite o número da opção desejada:");
     }
+
     public static void resgate() {
-        System.out.println("Resgate" + "\n");
+        System.out.println("Resgate");
 
     }
+
     public static void deposito() {
-        System.out.println("Deposito" + "\n");
+        System.out.println("Depósito");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor a ser depositado: ");
+        double valor = scanner.nextDouble();
+
+        cliente.depositar(valor); // Supondo que a classe Conta tenha um método depositar() para adicionar o valor à conta
+
+        System.out.println("Depósito efetuado com sucesso!");
     }
+
     public static void visualizar() {
-        System.out.println("Visualizar Dados da Conta" + "\n");
+        System.out.println("Visualizar Dados da Conta");
+        System.out.println("Nome do Cliente: " + cliente.getNome());
+        System.out.println("Saldo: " + cliente.getSaldo());
     }
+
     public static void exibirsaldo() {
-        System.out.println("Saldo" + "\n");
+        System.out.println("Saldo");
+        System.out.println("Saldo: " + cliente.getSaldo());
     }
+
     public static void investimento() {
-        System.out.println("Investimento" + "\n");
+        System.out.println("Investimento");
+
     }
+
     public static void bonus() {
-        System.out.println("Bonus" + "\n");
+        System.out.println("Bônus");
+
     }
-
-      /*  Conta cliente = new Conta("Nome do Cliente", 100);
-
-        System.out.println("Saldo atual: " + cliente.getSaldoDisponivel());
-
-        // Realizando operações de depósito e resgate
-        cliente.depositar(40);
-        cliente.resgatar(30);
-
-        System.out.println("Saldo atual: " + cliente.getSaldoDisponivel());*/
-    }
+}
