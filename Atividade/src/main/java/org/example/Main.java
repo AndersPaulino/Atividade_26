fd78bf5b6038da225810f2137384e2d0e2e42e57
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-07-26 21:07:22 -0300

    Teste do código na Classe Main.

--- Atividade/src/main/java/org/example/Main.java
+++ Atividade/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.entity.Conta;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conta cliente = new Conta("Nome do Cliente", 100);
+
+        System.out.println("Saldo atual: " + cliente.getSaldoDisponivel());
+
+        // Realizando operações de depósito e resgate
+        cliente.depositar(50);
+        cliente.resgatar(30);
+
+        System.out.println("Saldo atual: " + cliente.getSaldoDisponivel());
     }
 }
